5b2136326483938a18e7f174548fdf456bbeef23
MaritnRep <repickym@gmail.com>
2020-03-07 15:46:13 +0000

    rock64 working version

--- download_reader.py
+++ download_reader.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
@@ -6,21 +8,34 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import TimeoutException
 
+# Command to hide browser window when executing
 import os
 os.environ['MOZ_HEADLESS'] = '1'
 
-browser = webdriver.Firefox(executable_path='./geckodriver')
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_argument('--headless')
+chrome_options.add_argument('--no-sandbox')
+chrome_options.add_argument('--disable-dev-shm-usage')
+browser = webdriver.Remote(
+    command_executor='http://localhost:4444/wd/hub',
+    desired_capabilities =
+		{
+			'waitForReady': True,
+			'applicationType': 'Device'
+        },
+    options=chrome_options)
 
-browser.get('http://192.168.1.254')
+try:
+    browser.get('http://192.168.1.254')
+    password_input = WebDriverWait(browser, 10).until(EC.element_to_be_clickable((By.ID, 'login_password')))
+    password_input.click()
+    password_input.send_keys('# ROUTER PASSWORD #' + Keys.RETURN)
+    down_usage = WebDriverWait(browser, 10).until(EC.element_to_be_clickable((By.ID, 'home_total_down_flow')))
+    whole_text = down_usage.text
+    print("Download usage in details:{} {}".format(whole_text[:-2], whole_text[-2:]))
+    logout_button = WebDriverWait(browser, 10).until(EC.element_to_be_clickable((By.XPATH, "//div[@onclick='EMUI.LogoutObjController.Logout();']")))
+    logout_button.click()
+    browser.quit()
+except Exception as e:
+    print(e)
 
-password_input = WebDriverWait(browser, 20).until(EC.element_to_be_clickable((By.ID, 'login_password')))
-password_input.click()
-password_input.send_keys('PASSWORD' + Keys.RETURN)
-
-down_usage = WebDriverWait(browser, 20).until(EC.element_to_be_clickable((By.ID, 'home_total_down_flow')))
-whole_text = down_usage.text
-print(f'Download usage: {whole_text} In details: {whole_text[:-2]} {whole_text[-2:]}')
-
-logout = WebDriverWait(browser, 20).until(EC.element_to_be_clickable((By.XPATH, "//div[@onclick='EMUI.LogoutObjController.Logout();']"))).click()
-
-browser.quit()
