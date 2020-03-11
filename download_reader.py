46830cc155295fe97995982cc3b4e8161f61bcd3
MaritnRep <repickym@gmail.com>
2020-03-11 20:31:09 +0000

    Exception mitigation

--- download_reader.py
+++ download_reader.py
@@ -10,6 +10,8 @@ from selenium.common.exceptions import TimeoutException
 
 import mysql.connector as mariadb
 
+from time import sleep
+
 # Command to hide browser window when executing
 import os
 os.environ['MOZ_HEADLESS'] = '1'
@@ -22,29 +24,35 @@ browser = webdriver.Remote(
     command_executor='http://192.168.1.1:4444/wd/hub',
     options=chrome_options)
 
-try:
-    browser.get('http://192.168.1.254')
-    password_input = WebDriverWait(browser, 10).until(EC.element_to_be_clickable((By.ID, 'login_password')))
-    password_input.click()
-    password_input.send_keys('##ROUTER PASSWORD##' + Keys.RETURN)
-    WebDriverWait(browser, 10).until(EC.element_to_be_clickable((By.ID, "menu_top_tools"))).click()
-    WebDriverWait(browser, 10).until(EC.element_to_be_clickable((By.XPATH, "//div/div[3]/div"))).click()
-    down_usage = WebDriverWait(browser, 10).until(EC.element_to_be_clickable((By.ID, 'statistic_total_has_used')))
-    whole_text = down_usage.text
-    WebDriverWait(browser, 10).until(EC.element_to_be_clickable((By.XPATH, "//div[@onclick='EMUI.LogoutObjController.Logout();']"))).click()
-    browser.quit()
-    mariadb_connection = mariadb.connect(user='worker', database='mysql', host='192.168.1.1')
-    cursor = mariadb_connection.cursor(buffered=True)
-    cursor.execute("SHOW TABLES LIKE 'dl_usage'")
-    if(cursor.rowcount < 1):
-        cursor.execute("CREATE TABLE dl_usage( measurement_id INT(11) NOT NULL AUTO_INCREMENT,  timestamp TIMESTAMP,  value FLOAT(5,1), unit VARCHAR(2), CONSTRAINT measurement_pk PRIMARY KEY (measurement_id))")
-    sql = "INSERT INTO dl_usage (value, unit) VALUES (%s, %s)"
-    val = [float(whole_text[:-2]), str(whole_text[-2:])]
-    cursor.execute(sql, val)
-    mariadb_connection.commit()
-    cursor.close()
-except Exception as e:
-    print(e)
+successful = False
+while(not successful):
+    try:
+        browser.get('http://192.168.1.254')
+        password_input = WebDriverWait(browser, 10).until(EC.element_to_be_clickable((By.ID, 'login_password')))
+        password_input.click()
+        password_input.send_keys('##ROUTER PASSWORD##' + Keys.RETURN)
+        WebDriverWait(browser, 10).until(EC.element_to_be_clickable((By.ID, "menu_top_tools"))).click()
+        WebDriverWait(browser, 10).until(EC.element_to_be_clickable((By.XPATH, "//div/div[3]/div"))).click()
+        down_usage = WebDriverWait(browser, 10).until(EC.element_to_be_clickable((By.ID, 'statistic_total_has_used')))
+        whole_text = down_usage.text
+        WebDriverWait(browser, 10).until(EC.element_to_be_clickable((By.XPATH, "//div[@onclick='EMUI.LogoutObjController.Logout();']"))).click()
+        browser.quit()
+        mariadb_connection = mariadb.connect(user='worker', database='mysql', host='192.168.1.1')
+        cursor = mariadb_connection.cursor(buffered=True)
+        cursor.execute("SHOW TABLES LIKE 'dl_usage'")
+        if(cursor.rowcount < 1):
+            cursor.execute("CREATE TABLE dl_usage( measurement_id INT(11) NOT NULL AUTO_INCREMENT,  timestamp TIMESTAMP,  value FLOAT(5,1), unit VARCHAR(2), CONSTRAINT measurement_pk PRIMARY KEY (measurement_id))")
+
+        sql = "INSERT INTO dl_usage (value, unit) VALUES (%s, %s)"
+        val = [float(whole_text[:-2]), str(whole_text[-2:])]
+        cursor.execute(sql, val)
+        mariadb_connection.commit()
+        cursor.close()
+    except Exception as e:
+        print(e)
+        sleep(60)
+    else:
+        successful = True
 
 
 
